ec9141c2433f405da35ca6599d55b2c6aeea0a6f
ari-barnett <72417784+ari-barnett@users.noreply.github.com>
2021-06-12 19:03:25 -0400

    Add files via upload

---
+++ sub_domainPositions.py
@@ -0,0 +1,38 @@
+import math
+
+origin_point = 11
+sub_domain = 4
+N = 5
+yDim = 10
+i_values = []
+
+'''
+sub_domain = desired number of sub domains 
+N = length of a given sub domain 
+M = N * yDim
+
+For a board of domain size (K = 100) determine the lower left corner of a fractination of 9
+'''
+
+i = 0
+multiplier = 0
+while i < math.sqrt(sub_domain):
+    i_values.append(origin_point + (multiplier * N))
+    i += 1
+    multiplier += 1
+
+holding = []
+for i in i_values:
+    q = 1
+    while q < (math.sqrt(sub_domain)):
+        holding.append(i + (q * (N * yDim)))
+        q += 1
+
+for i in holding:
+    i_values.append(i)
+
+i_values.sort()
+print(i_values)
+print(len(i_values))
+
+
